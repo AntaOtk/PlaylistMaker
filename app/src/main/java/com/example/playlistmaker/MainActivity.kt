855cd14c353446751b99a1de00e20ab721231ea4
AntaOtk <ovaorange@yandex.ru>
2023-02-26 14:33:29 +0100

    call activity

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.playlistmaker
-
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,25 +12,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val find_button = findViewById<Button>(R.id.search_button)
-        val library_button = findViewById<Button>(R.id.library_button)
-        val settigs_button = findViewById<Button>(R.id.settings_button)
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        val settigsButton = findViewById<Button>(R.id.settings_button)
 
-        find_button.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск!", Toast.LENGTH_SHORT).show()
+        searchButton.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+
         }
 
-        library_button.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека!", Toast.LENGTH_SHORT).show()
+        libraryButton.setOnClickListener {
+            val displayIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(displayIntent)
         }
 
         val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на кнопку!", Toast.LENGTH_SHORT).show()
+                val displayIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+                startActivity(displayIntent)
             }
         }
 
-        settigs_button.setOnClickListener(buttonClickListener)
+        settigsButton.setOnClickListener(buttonClickListener)
 
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,10 @@
+package com.example.playlistmaker
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
