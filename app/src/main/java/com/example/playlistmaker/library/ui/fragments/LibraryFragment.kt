02728481c0162745c636785d4bc4dd7a37eb4829
Anta <ovaorange@yandex.ru>
2023-08-21 07:33:25 +0200

    19 sprint finish

--- app/src/main/java/com/example/playlistmaker/library/ui/fragments/LibraryFragment.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/fragments/LibraryFragment.kt
@@ -5,13 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentLibraryBinding
 import com.example.playlistmaker.library.ui.adapter.LibraryViewPagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 
 class LibraryFragment : Fragment() {
 
-    private lateinit var binding: FragmentLibraryBinding
+    private var binding: FragmentLibraryBinding? = null
     private lateinit var tabMediator: TabLayoutMediator
 
     override fun onCreateView(
@@ -20,17 +21,17 @@ class LibraryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLibraryBinding.inflate(inflater, container, false)
-        return binding.root
+        return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        binding.viewPager.adapter = LibraryViewPagerAdapter(childFragmentManager, lifecycle)
+        binding?.viewPager?.adapter = LibraryViewPagerAdapter(childFragmentManager, lifecycle)
 
-        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+        tabMediator = TabLayoutMediator(binding!!.tabLayout, binding!!.viewPager) { tab, position ->
             when (position) {
-                0 -> tab.text = "Избранные треки"
-                1 -> tab.text = "Плейлисты"
+                0 -> tab.text = R.string.my_track.toString()
+                1 -> tab.text = R.string.playlists.toString()
             }
         }
         tabMediator.attach()
